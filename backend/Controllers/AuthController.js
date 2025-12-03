3cf0db9f952450b4761214c37c0df2c21ec42d76
Aayush Bhatt <152008288+Aayushbhatt06@users.noreply.github.com>
2025-12-04 00:35:42 +0530

    correct hardcoded fetch for logout

--- backend/Controllers/AuthController.js
+++ backend/Controllers/AuthController.js
@@ -97,7 +97,7 @@ const logout = (req, res) => {
   res.clearCookie("jwt", {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
-    sameSite: "strict",
+    sameSite: "none",
   });
 
   res.status(200).json({
