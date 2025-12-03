8a2b912262e0d318f6bdf77ab0d7648c77caee5e
Aayush Bhatt <152008288+Aayushbhatt06@users.noreply.github.com>
2025-12-04 00:05:13 +0530

    add vercel.json for routes error in deployment

--- backend/Models/db.js
+++ backend/Models/db.js
@@ -1,9 +1,15 @@
-const mongoose = require('mongoose');
-main().catch(err => console.log(err));
-const mongo_url = process.env.MONGO_CONN;
-async function main() {
-  await mongoose.connect("mongodb://localhost:27017/SkillBarter")
-  .then(console.log("Connected to Mongo db"))
+const mongoose = require("mongoose");
+require("dotenv").config();
 
-  // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
+const mongo_url = process.env.MONGO_CONN;
+
+async function main() {
+  try {
+    await mongoose.connect(mongo_url);
+    console.log("Connected to MongoDB");
+  } catch (err) {
+    console.error("MongoDB connection failed:", err);
+  }
 }
+
+main();
